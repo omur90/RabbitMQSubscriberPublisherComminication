8286591e324e89b08d6b4e3fa6d360d678bce64f
omur.kurt <omur90@gmail.com>
2022-09-05 00:50:33 +0300

    rabbitmq subscriber and publisher completed

---
+++ RabbitMQBasicCommunication/RabbitMQBasicCommunication.Publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    Uri = new Uri("amqps://vxmxyzmj:yBx86MiH5XnT5vXmEgVF2InBkDkJp6P2@sparrow.rmq.cloudamqp.com/vxmxyzmj")
+};
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+var queueName = "hello-queue";
+
+// durable = false save message in memory, set true save physically in rabbitMQ Server
+//exclusive = set true acces only this channel, set false access with any subscriber.
+//autoDelete = set true when all subscriber disconnect then message delete. Best practice says set false.
+channel.QueueDeclare(queueName, true, false, false);
+
+// message is a byte array for RabbitMQ.
+string message = "hello word !";
+
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty, queueName, null, messageBody);
+
+Console.WriteLine("Message sent successfully !");
+
+Console.ReadLine();
---
+++ RabbitMQBasicCommunication/RabbitMQBasicCommunication.Subscriber/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    Uri = new Uri("amqps://vxmxyzmj:yBx86MiH5XnT5vXmEgVF2InBkDkJp6P2@sparrow.rmq.cloudamqp.com/vxmxyzmj")
+};
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+var queueName = "hello-queue";
+
+//channel.QueueDeclare(queueName, true, false, false); => already created Publisher Library.  Than it is not necessary !
+
+var consumer = new EventingBasicConsumer(channel);
+
+//autoAck : set false when this message read it after automatically delete. set true after delete yourself 
+channel.BasicConsume(queueName, autoAck: true,consumer);
+
+consumer.Received += (object? sender, BasicDeliverEventArgs e) => {
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Console.WriteLine($"Recived message : {message}");
+};
+
+Console.ReadLine();
